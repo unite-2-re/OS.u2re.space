66576e79b19c0187715d7512da19ccc6aaa56e85
unit-404 <conway24b@gmail.com>
2024-11-01 15:34:26 +0700

    potentially external mode of lit

--- src/$comp$/$lit$/Example.ts
+++ src/$comp$/$lit$/Example.ts
@@ -1,7 +1,16 @@
-import {LitElement, html, css} from 'lit';
+
+// EXTERNAL MODE!
+/// <reference types="lit" />
+// @ts-ignore
+//import {LitElement, html, css} from '/externals/vendor/lit-core.min.js';
+
+// internal package mode (will or chunk or built-in)
+import {LitElement, html, css} from "lit";
+
+// you won't able to exclude that
 import {customElement, property} from 'lit/decorators.js';
 
-//
+// @ts-ignore
 @customElement('simple-greeting')
 export class SimpleGreeting extends LitElement {
     // Define scoped styles right with your component, in plain CSS
