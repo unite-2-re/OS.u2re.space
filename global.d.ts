541258666dcbcdefef16cbc45fed1d4c632c1025
unit-404 <conway24b@gmail.com>
2024-11-17 18:59:17 +0700

    testing observer

--- global.d.ts
+++ global.d.ts
@@ -14,3 +14,11 @@ declare module "*.html?raw"
     const content: string;
     export default content;
 }
+
+declare module "solid-js" {
+    namespace JSX {
+        interface Directives {
+            observe: [() => any, (v: any) => any]
+        }
+    }
+}
--- src/$hash$/settings/$solid$/Tabbed.tsx
+++ src/$hash$/settings/$solid$/Tabbed.tsx
@@ -1,6 +1,9 @@
-//
+// @ts-ignore
 import { For, createSignal, onMount  } from "solid-js";
 import html from "solid-js/html";
+
+// @ts-ignore
+import {observeAttribute} from "/externals/lib/dom.js";
 
 //
 export interface TabType {
@@ -21,17 +24,31 @@ const refAndMount = (cb)=>{
     }
 }
 
+//
+const observe = (val) => {
+    return (el)=> {
+        const [attr, setter] = val;
+        observeAttribute(el, attr, (obs)=>setter(el.getAttribute(attr)));
+    }
+}
+
+//
+const logged = (fx)=>{
+    return (...args)=>{
+        console.log(...args);
+        return fx(...args);
+    }
+}
+
 // while: tab.component should be  ()=> html`...`
 export const Tabbed = ({tabs}: TabProps) => {
     const [currentTab, setTab] = createSignal("main");
-
-    //
     const $element = refAndMount((topLevel)=> {
         console.log(topLevel.querySelector("input"));
     });
 
     //
-    return html`<div ref=${$element}>
+    return html`<div ref=${$element} data-tab=${currentTab} ref=${observe(["data-tab", logged(setTab)])}>
         <div>
             <${For} each=${() => tabs}>${(tab) => {
                 return html`<label>
