2df7acfe49acf2ab5c12bfe2d5d4ff02b5a2cbcb
u2re-dev <conway24b@gmail.com>
2024-12-07 20:01:20 +0700

    don't hide modal by inputs

--- src/$core$/Modal.ts
+++ src/$core$/Modal.ts
@@ -2,7 +2,7 @@ export const initializeModal = ()=>{
     //
     const hideModal = (ev)=>{
         const element = ev?.target as HTMLElement;
-        const selector = ".adl-modal, .u2-input, input, ui-contextmenu";
+        const selector = ".adl-modal, .u2-input, input, ui-contextmenu, button, ui-focustext";
         const modals = document.querySelectorAll(".adl-modal:not([data-hidden])");
         if (!(element.matches(selector) || element.closest(selector))) {
             modals.forEach((m)=>m?.setAttribute?.("data-hidden", ""));
