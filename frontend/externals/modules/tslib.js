c559030d58c4b1d568416db1d72a53c3550a0b0c
u2re-dev <c24b@u2re.ru>
2025-01-10 05:59:06 +0700

    setup environment and redirect from '/'

--- frontend/externals/modules/tslib.js
+++ frontend/externals/modules/tslib.js
@@ -1,1 +1,1 @@
-../../../../ui.system/dist/modules/tslib.js
+function e(e,t,f,c){var o,l,n=arguments.length,r=3>n?t:null===c?c=Object.getOwnPropertyDescriptor(t,f):c;if("object"==typeof Reflect&&"function"==typeof Reflect.decorate)r=Reflect.decorate(e,t,f,c);else for(l=e.length-1;l>=0;l--)(o=e[l])&&(r=(3>n?o(r):n>3?o(t,f,r):o(t,f))||r);return n>3&&r&&Object.defineProperty(t,f,r),r}export{e as _};
