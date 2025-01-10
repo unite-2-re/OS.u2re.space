590d8d3e6b53634d033773dcebdc402b4fec4b68
u2re-dev <c24b@u2re.ru>
2025-01-10 20:57:50 +0700

    ui-modal not compatible with keyboard

--- src/$core$/Modal.ts
+++ src/$core$/Modal.ts
@@ -3,9 +3,9 @@ export const initializeModal = ()=>{
     const hideModal = (evc)=>{
         const ev = evc?.detail || evc;
         const element = ev?.target as HTMLElement;
-        const selector = ".adl-modal, .u2-input, input, ui-contextmenu, button, ui-focustext";
+        const selector = ".adl-modal, .u2-input, input, ui-contextmenu, button, ui-focustext, ui-modal, ui-longtext";
         const modals = document.querySelectorAll(".adl-modal:not([data-hidden])");
-        if (!(element.matches(selector) || element.closest(selector))) {
+        if (!(element.matches(selector) || element.closest(selector) || document.activeElement?.matches?.("input"))) {
             modals.forEach((m)=>m?.setAttribute?.("data-hidden", ""));
         }
     }
--- src/$solid$/workspace/ItemEdit.tsx
+++ src/$solid$/workspace/ItemEdit.tsx
@@ -66,7 +66,7 @@ export const ItemEdit = ({
     }
 
     //
-    return html`<ui-modal data-hidden class="adl-modal" data-alpha="1" data-scheme="solid">
+    return html`<div data-hidden class="adl-modal" data-alpha="1" data-scheme="solid">
         <form data-alpha="0" data-highlight="0" class="adl-item-edit" ref=${$content}>
             <${For} each=${() => form}>${(input) => { return html`<label>
                 <div class="adl-label">${input?.label}</div>
@@ -78,7 +78,7 @@ export const ItemEdit = ({
             <button onClick=${deleteA} class="adl-delete" data-scheme="inverse" data-chroma="0.05"> Delete </button>
             <button onClick=${confirm} class="adl-confirm" data-scheme="inverse" data-chroma="0.05"> Confirm </button>
         </div>
-    </div>`;
+    </ui-modal>`;
 };
 
 //
