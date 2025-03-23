ee427ebd43acc0cdb71b772dd40a8864ee1f8ed9
u2re-dev <c24b@u2re.ru>
2025-03-23 13:18:32 +0700

    animate drop menu selection

--- src/$blue$/workspace/ItemEdit.ts
+++ src/$blue$/workspace/ItemEdit.ts
@@ -10,12 +10,12 @@ const fields = ["label", "icon", "href", "action", "id"];
 const fieldTypes = new Map([
     ["text", (input: any) => H(`<ui-longtext data-scheme="solid" data-alpha="0" data-chroma="0.01" data-highlight="3" data-highlight-hover="1" class="u2-input" data-name=${input?.name}><input value="" placeholder=${input?.name} name=${input?.name} type="text" label="test" placeholder="test-longtext" tabindex="0" draggable="false" autocomplete="off" class="u2-input" scroll="no"/></ui-longtext>`)],
     ["action-list", (input: any) => H(`<ui-button data-scheme="solid" data-alpha="0" data-chroma="0.01" data-highlight="3" data-highlight-hover="1" class="u2-input" data-name=${input?.name} style="block-size: 2rem;">
-    <ui-button-row data-alpha="0" data-value="open-link"> <ui-icon icon="external-link"></ui-icon> <span>Open Link</span> </ui-button-row>
-    <ui-button-row data-alpha="0" data-value="open-in-frame"> <ui-icon icon="app-window"></ui-icon> <span>Open in Frame</span> </ui-button-row>
-    <ui-button-row data-alpha="0" data-value="manager"> <ui-icon icon="folder"></ui-icon> <span>Explore</span> </ui-button-row>
-    <ui-button-row data-alpha="0" data-value="settings"> <ui-icon icon="settings"></ui-icon> <span>Settings</span> </ui-button-row>
-    <ui-button-row data-alpha="0" data-value="use-as-wallpaper"> <ui-icon icon="wallpaper"></ui-icon> <span>Use as Wallpaper</span> </ui-button-row>
-    <ui-button-row data-alpha="0" data-value="copy-link"> <ui-icon icon="copy-minus"></ui-icon> <span>Copy Link</span> </ui-button-row>
+    <ui-button-row data-highlight-hover="2" data-alpha="0" data-value="open-link"> <ui-icon icon="external-link"></ui-icon> <span>Open Link</span> </ui-button-row>
+    <ui-button-row data-highlight-hover="2" data-alpha="0" data-value="open-in-frame"> <ui-icon icon="app-window"></ui-icon> <span>Open in Frame</span> </ui-button-row>
+    <ui-button-row data-highlight-hover="2" data-alpha="0" data-value="manager"> <ui-icon icon="folder"></ui-icon> <span>Explore</span> </ui-button-row>
+    <ui-button-row data-highlight-hover="2" data-alpha="0" data-value="settings"> <ui-icon icon="settings"></ui-icon> <span>Settings</span> </ui-button-row>
+    <ui-button-row data-highlight-hover="2" data-alpha="0" data-value="use-as-wallpaper"> <ui-icon icon="wallpaper"></ui-icon> <span>Use as Wallpaper</span> </ui-button-row>
+    <ui-button-row data-highlight-hover="2" data-alpha="0" data-value="copy-link"> <ui-icon icon="copy-minus"></ui-icon> <span>Copy Link</span> </ui-button-row>
 </ui-button>`)]
 ]);
 
